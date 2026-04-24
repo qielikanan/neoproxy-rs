05cf3a0b22b236ae6b4fabc01bc2b3b6ea68f957
qielikanan <kaqielina@gmail.com>
2026-04-24 19:51:01 +0800

    fix

--- src/l4_security/server.rs
+++ src/l4_security/server.rs
@@ -13,13 +13,15 @@ pub fn build_tls_acceptor<P: AsRef<Path>>(
 ) -> io::Result<Arc<ServerConfig>> {
     let certs = load_certs(cert_path)?;
     let key = load_private_key(key_path)?;
-    let config = ServerConfig::builder()
+    let mut config = ServerConfig::builder()
         .with_no_client_auth()
         .with_single_cert(certs, key)
         .map_err(|e| io::Error::new(io::ErrorKind::InvalidInput, e.to_string()))?;
+
+    config.alpn_protocols = vec![b"h2".to_vec(), b"http/1.1".to_vec()];
+
     Ok(Arc::new(config))
 }
-
 pub fn build_tls_acceptor_jls<P: AsRef<Path>>(
     cert_path: P,
     key_path: P,
@@ -41,10 +43,13 @@ pub fn build_tls_acceptor_jls<P: AsRef<Path>>(
 }
 pub fn build_tls_acceptor_generated(domain: &str) -> io::Result<Arc<ServerConfig>> {
     let (certs, key) = generate_dummy_cert(domain)?;
-    let config = ServerConfig::builder()
+    let mut config = ServerConfig::builder()
         .with_no_client_auth()
         .with_single_cert(certs, key)
         .map_err(|e| io::Error::new(io::ErrorKind::InvalidInput, e.to_string()))?;
+
+    config.alpn_protocols = vec![b"h2".to_vec(), b"http/1.1".to_vec()];
+
     Ok(Arc::new(config))
 }
 
